cc47e2f8f9e3f64863dfbf7365eb5e1be222184d
MaishaFahmida <60548382+MaishaFahmida@users.noreply.github.com>
2024-06-27 12:20:33 +0200

    Update tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,24 +1,4 @@
 #!/bin/bash
 # Run the data pipeline
 
-chmod +x project/tests.sh
-echo "Running the data pipeline..."
-./pipeline.sh
-
-# Check if the output file exists
-#OUTPUT_FILE="../data/data_base.db"
-OUTPUT_FILE="sqlite:///../data/Final_Data.db"
-#sqlite:///../data/Final_Data.db
-
-if [ -f "$OUTPUT_FILE" ]; then
-    echo "Database file exists: $OUTPUT_FILE"
-else
-    echo "Error: Database file  not found!"
-    exit 1
-fi
-
-# Run the Python tests
-echo "Running the Python tests..."
-pytest tests.py
-
-echo "All tests completed!"
+python ./project/tests.py
